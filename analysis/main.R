4423bcc3228d381e279b2f9afe924557fa105211
Haruo <haruo-mtok@outlook.com>
2023-03-04 20:50:24 +0900

    綺麗に

--- analysis/main.R
+++ analysis/main.R
@@ -17,23 +17,16 @@ arrival_hist <- function(k) {
     df_freq
     ggplot(df_freq, aes(着順, 頻度, fill = fill, label = label)) +
         geom_bar(stat = "identity") +
-        labs(subtitle = str_c(k, "番人気ha?👸")) +
+        labs(subtitle = str_c(k, "番人気")) +
         scale_fill_manual(values = c("TRUE" = "tomato", "FALSE" = "gray"), guide = "none") +
         geom_text(hjust = -0.3, vjust = 1.5)
 }
-# arrival_hist(1)
+
 plist <- map(1:6, arrival_hist)
-pp <- wrap_plots(plist, ncol = 2, nrow = 3, byrow = FALSE)
-pp <- pp + plot_annotation(
-    subtitle = "隴西の李徴は博学才穎、天宝の末年、若くして名を虎榜に連ね、ついで江南尉に補せられたが、性、狷介、自ら恃むところ頗る厚く、賤吏に甘んずるを潔しとしなかった。",
-    caption = "超级反派凌辰（化名潘洛斯）在和死对头超级英雄叶子暮的激战中意外穿越回2030年的高二"
-) & mytheme()
+pp <- wrap_plots(plist, ncol = 2, nrow = 3, byrow = FALSE) +
+    plot_annotation(
+        subtitle = "期間[2022年〜2022年]\n競馬場[札幌、函館、福島、新潟、東京、中山、中京、京都、阪神、小倉]\nクラス[G1、G2、G3]"
+    ) & mytheme()
+
 # ggsave("figure/人気の的中率.pdf", pp, device = cairo_pdf)
-ggsave(
-    "figure/人気の的中率.svg",
-    plot = pp,
-    device = svglite,
-    # system_fonts = list(sans = "Kiwi Maru", symbol = "Kiwi Maru"),
-    # web_fonts = "https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap",
-    fix_text_size = FALSE,
-)
+pp |> ggsave_svg("figure/人気の的中率.svg")
