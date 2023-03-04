3aac6f915ad7426c1c1e16916dd553e36ea05b7c
Haruo <haruo-mtok@outlook.com>
2023-03-04 14:05:28 +0900

    purify

--- analysis/main.R
+++ analysis/main.R
@@ -1,28 +1,8 @@
 library(tidyverse)
 library(patchwork)
 
+df <- read_csv("data/df.csv")
 
-df_search_result <- read_csv("data/search/期間[2022年〜2022年]、競馬場[札幌、函館、福島、新潟、東京、中山、中京、京都、阪神、小倉]、クラス[G1、G2、G3].csv")
-df_search_result
-
-
-ids <- df_search_result[["レースID"]]
-dfs <- list()
-for (id in ids) {
-    fname <- str_c("data/race/", id, "/レース結果.csv")
-    df <- read_csv(fname, progress = FALSE, show_col_types = FALSE)
-    suppressWarnings(df["着順"] <- as.double(df$着順)) # 「中」「除」をNAに
-    df["通過"] <- as.character(df$通過)
-    suppressWarnings(df["単勝"] <- as.double(df$単勝))
-    df["レースID"] <- id
-    dfs <- c(dfs, list(df))
-}
-df <- bind_rows(dfs) |>
-    mutate(着順 = factor(着順)) |>
-    select(レースID, everything())
-
-df
-# df |> View()
 
 arrival_hist <- function(k) {
     df_freq <- df |>
@@ -38,7 +18,7 @@ arrival_hist <- function(k) {
     df_freq
     ggplot(df_freq, aes(着順, 頻度, fill = fill, label = label)) +
         geom_bar(stat = "identity") +
-        labs(title = str_c(k, "番人気")) +
+        labs(subtitle = str_c(k, "番人気")) +
         scale_fill_manual(values = c("TRUE" = "tomato", "FALSE" = "gray")) +
         geom_text(hjust = -0.3, vjust = 1.5) +
         theme(
